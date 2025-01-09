c398c0b1b4263f7a45f99427495bbec4bedd4bdf
Muxsimov <muxsimovasadbek665@gmail.com>
2025-01-09 19:41:16 +0500

    finished

---
+++ src/Companent/home/app.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function app() {
+  return (
+    <div></div>
+  )
+}
+
+export default app
--- src/Companent/home/index.jsx
+++ src/Companent/home/index.jsx
@@ -1,95 +1,186 @@
 import React from "react";
 import yulduz from "../../assets/yulduz.svg";
 import savat from "../../assets/savat.png";
+import like from "../../assets/like.png";
+import like1 from "../../assets/like1.png";
+import { useState } from 'react'
 import "../home/stayl.css";
+
+function userId() {
+  return Date.now().toString(36) + Math.random().toString(36).substring(2);
+}
+
+
 function Home() {
   const data = [
     {
-      id: 1,
-      imgs: "https://object.pscloud.io/cms/cms/Uploads/30_433626.jpg",
-      name: "Smartfon Xiaomi Redmi Note 13,6/128 GB, 8/128 GB, 8/256GB,",
+      id: userId(),
+      desc: `Ofis texnikalari uchun qog'oz varaqlari Sylvamo Svetotopy, A4f`,
+      ball: '4.9 (21748ta sharh)',
+      bolbt: `2 975 so'm/oyiga`,
+      skidka: '55 000',
+      narx: `42 000 so'm`,
+      img: 'https://images.uzum.uz/ctg2kjdpb7faledbod7g/original.jpg'
     },
     {
-      id: 2,
-      name: "Smartfon Xiaomi Redmi Note 13,6/128 GB, 8/128 GB, 8/256GB,",
-      imgs: "https://object.pscloud.io/cms/cms/Uploads/30_433626.jpg",
+      id: userId(),
+      desc: `Ofis texnikalari uchun qog'oz varaqlari Sylvamo Svetotopy, A4f`,
+      ball: '4.9 (21748ta sharh)',
+      bolbt: `2 975 so'm/oyiga`,
+      skidka: '55 000',
+      narx: `42 000 so'm`,
+      img: 'https://images.uzum.uz/ctg2kjdpb7faledbod7g/original.jpg'
     },
     {
-      id: 3,
-      name: "Smartfon Xiaomi Redmi Note 13,6/128 GB, 8/128 GB, 8/256GB,",
-      imgs: "https://object.pscloud.io/cms/cms/Uploads/30_433626.jpg",
+      id: userId(),
+      desc: `Ofis texnikalari uchun qog'oz varaqlari Sylvamo Svetotopy, A4f`,
+      ball: '4.9 (21748ta sharh)',
+      bolbt: `2 975 so'm/oyiga`,
+      skidka: '55 000',
+      narx: `42 000 so'm`,
+      img: 'https://images.uzum.uz/ctg2kjdpb7faledbod7g/original.jpg'
     },
     {
-      id: 4,
-      name: "Smartfon Xiaomi Redmi Note 13,6/128 GB, 8/128 GB, 8/256GB,",
-      imgs: "https://object.pscloud.io/cms/cms/Uploads/30_433626.jpg",
+      id: userId(),
+      desc: `Ofis texnikalari uchun qog'oz varaqlari Sylvamo Svetotopy, A4f`,
+      ball: '4.9 (21748ta sharh)',
+      bolbt: `2 975 so'm/oyiga`,
+      skidka: '55 000',
+      narx: `42 000 so'm`,
+      img: 'https://images.uzum.uz/ctg2kjdpb7faledbod7g/original.jpg'
     },
     {
-      id: 5,
-      name: "Smartfon Xiaomi Redmi Note 13,6/128 GB, 8/128 GB, 8/256GB,",
-      imgs: "https://object.pscloud.io/cms/cms/Uploads/30_433626.jpg",
+      id: userId(),
+      desc: `Ofis texnikalari uchun qog'oz varaqlari Sylvamo Svetotopy, A4f`,
+      ball: '4.9 (21748ta sharh)',
+      bolbt: `2 975 so'm/oyiga`,
+      skidka: '55 000',
+      narx: `42 000 so'm`,
+      img: 'https://images.uzum.uz/ctg2kjdpb7faledbod7g/original.jpg'
     },
     {
-      id: 6,
-      name: "Smartfon Xiaomi Redmi Note 13,6/128 GB, 8/128 GB, 8/256GB,",
-      imgs: "https://object.pscloud.io/cms/cms/Uploads/30_433626.jpg",
+      id: userId(),
+      desc: `Ofis texnikalari uchun qog'oz varaqlari Sylvamo Svetotopy, A4f`,
+      ball: '4.9 (21748ta sharh)',
+      bolbt: `2 975 so'm/oyiga`,
+      skidka: '55 000',
+      narx: `42 000 so'm`,
+      img: 'https://images.uzum.uz/ctg2kjdpb7faledbod7g/original.jpg'
     },
     {
-      id: 7,
-      name: "Smartfon Xiaomi Redmi Note 13,6/128 GB, 8/128 GB, 8/256GB,",
-      imgs: "https://object.pscloud.io/cms/cms/Uploads/30_433626.jpg",
+      id: userId(),
+      desc: `Ofis texnikalari uchun qog'oz varaqlari Sylvamo Svetotopy, A4f`,
+      ball: '4.9 (21748ta sharh)',
+      bolbt: `2 975 so'm/oyiga`,
+      skidka: '55 000',
+      narx: `42 000 so'm`,
+      img: 'https://images.uzum.uz/ctg2kjdpb7faledbod7g/original.jpg'
     },
     {
-      id: 8,
-      name: "Smartfon Xiaomi Redmi Note 13,6/128 GB, 8/128 GB, 8/256GB,",
-      imgs: "https://object.pscloud.io/cms/cms/Uploads/30_433626.jpg",
+      id: userId(),
+      desc: `Ofis texnikalari uchun qog'oz varaqlari Sylvamo Svetotopy, A4f`,
+      ball: '4.9 (21748ta sharh)',
+      bolbt: `2 975 so'm/oyiga`,
+      skidka: '55 000',
+      narx: `42 000 so'm`,
+      img: 'https://images.uzum.uz/ctg2kjdpb7faledbod7g/original.jpg'
     },
     {
-      id: 9,
-      name: "Smartfon Xiaomi Redmi Note 13,6/128 GB, 8/128 GB, 8/256GB,",
-      imgs: "https://object.pscloud.io/cms/cms/Uploads/30_433626.jpg",
+      id: userId(),
+      desc: `Ofis texnikalari uchun qog'oz varaqlari Sylvamo Svetotopy, A4f`,
+      ball: '4.9 (21748ta sharh)',
+      bolbt: `2 975 so'm/oyiga`,
+      skidka: '55 000',
+      narx: `42 000 so'm`,
+      img: 'https://images.uzum.uz/ctg2kjdpb7faledbod7g/original.jpg'
     },
     {
-      id: 10,
-      name: "Smartfon Xiaomi Redmi Note 13,6/128 GB, 8/128 GB, 8/256GB,",
-      imgs: "https://object.pscloud.io/cms/cms/Uploads/30_433626.jpg",
+      id: userId(),
+      desc: `Ofis texnikalari uchun qog'oz varaqlari Sylvamo Svetotopy, A4f`,
+      ball: '4.9 (21748ta sharh)',
+      bolbt: `2 975 so'm/oyiga`,
+      skidka: '55 000',
+      narx: `42 000 so'm`,
+      img: 'https://images.uzum.uz/ctg2kjdpb7faledbod7g/original.jpg'
     },
-  ];
-  return (
-    <>
-      <div className="card_grid">
-        {data.map((itme) => {
-          return (
-            <div className="carta">
-              <div key={itme.id}>
-                <div>
-                  <img className=" imgs" src={itme.imgs} alt="" />
-                  <h2 className=" cart_text text-xs max-w-[200px] ">
-                    {itme.name}{" "}
-                  </h2>
-                  <div className=" flex text-xs ">
-                    <img src={yulduz} alt="" />
-                    < p className="  opacity-[70%]">4.9 (4092 sharsh)</p>
-                  </div>
-                  <p className="som">1 560 so'm/oyiga</p>
-                  <div className=" flex justify-between">
-                    <div>
-                      <p><del className="del opacity-[70%]  mt-8">3 010 000 so'm</del></p>
-                      <p className=" font-bold"> 2 499 000 so'm</p>
-                    </div>
-                    <div className="border">
-                    <img className=" p-3" src={savat} alt="" />
-                    </div>
-                  </div>
-                </div>
+  ]
 
-              </div>
-            </div>
-          );
-        })}
+
+  return (
+    <div>
+      <div className="cards-asosiy">
+        {data.map((item, index) => (
+          <UserCard key={index} data={item} />
+        ))}
       </div>
-    </>
+    </div>
   );
+
+}
+
+const UserCard = ({ data }) => {
+
+  const [bosildi, setBosildi] = useState("");
+
+
+  const [number, setNumber] = useState(0);
+  function decriment() {
+    setNumber((qosh) => (qosh === 0 ? 0 : qosh - 1));
+  }
+
+
+
+  return (
+    <div className="cards-asosiy">
+      <div key={data.id} className="cards">
+        <img src={data.img} alt={data.desc} className="products" />
+        <div>
+          <h3>{data.desc}</h3>
+          <div className="cards-sharh">
+            <img src={yulduz} alt="" />
+            <p>{data.ball}</p>
+          </div>
+          <span className="cards-bolbt">{data.bolbt}</span>
+          <div className="cards-narx">
+            <div className="cards-skidka">
+              <span className="skidka">{data.skidka || ''}</span>
+              <p>{data.narx}</p>
+            </div>
+            <div className="cards-count">
+              <button
+                onClick={() => {
+                  setNumber(number + 1);
+                }}
+                className="text-[20px] w-8 rounded-full">+
+              </button>
+              <h2 className="text-[26px]">{number}</h2>
+              <button
+                onClick={decriment}
+                className="text-[20px] w-8 rounded-full">-
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="like-content">
+        <img
+          onClick={() => {
+            if (bosildi) {
+              setBosildi(false);
+            } else {
+              setBosildi(true)
+            }
+          }}
+          className="like" src={
+            bosildi
+            ? "https://img.icons8.com/?size=100&id=83166&format=png&color=000000"
+            : "https://img.icons8.com/?size=100&id=82788&format=png&color=000000"
+
+          } alt="" />
+      </div>
+    </div>
+  );
+
 }
 
 export default Home;
